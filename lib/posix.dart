4c13ce27f8b312208e586232207be6a5bab11878
Brett Sutton <bsutton@noojee.com.au>
2020-09-20 19:19:51 +1000

    Added ffigen

--- lib/posix.dart
+++ lib/posix.dart
@@ -1,7 +1,9 @@
-export 'src/libc/libc.dart';
-export 'src/libc/seteuid.dart';
-export 'src/libc/setreuid.dart';
-export 'src/libc/geteuid.dart';
-export 'src/libc/getuid.dart';
+// export 'src/libc/libc.dart';
+// export 'src/libc/seteuid.dart';
+// export 'src/libc/setreuid.dart';
+// export 'src/libc/geteuid.dart';
+// export 'src/libc/getuid.dart';
 
 export 'src/posix_exception.dart';
+
+export 'src/posix_bindings.dart';
