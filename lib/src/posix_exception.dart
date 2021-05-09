15d3675d20168bb569407bf5bc31fa21c215e9cd
Brett Sutton <bsutton@noojee.com.au>
2021-05-09 22:52:06 +1000

    improved doco

--- lib/src/posix_exception.dart
+++ lib/src/posix_exception.dart
@@ -2,11 +2,13 @@ import 'package:posix/src/string/string.dart';
 
 class PosixException implements Exception {
   String message;
+
+  /// This should be the value returned by errno().
   int code;
 
   /// An optional value which allows the caller to over-ride the
-  /// standard error returned vi [sterror(error)]. If not supplied then
-  /// we call [strerror(error)] to get the posix error description.
+  /// standard error returned vi [sterror(errno)]. If not supplied then
+  /// we call [strerror(errno)] to get the posix error description.
   String? posixError;
   PosixException(this.message, this.code, {this.posixError});
 
