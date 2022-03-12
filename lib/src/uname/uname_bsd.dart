2d7405a03fd70e31dd9d2b05d84400fd4d2deac7
J-P Nurmi <jpnurmi@gmail.com>
2022-03-12 17:32:23 +0100

    Add uname()

---
+++ lib/src/uname/uname_bsd.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: constant_identifier_names
+
+part of 'uname.dart';
+
+const _UTSNAME_BSD_LENGTH = 256;
+
+class _utsname_bsd_t extends ffi.Struct {
+  @ffi.Array.multi([_UTSNAME_BSD_LENGTH])
+  external ffi.Array<ffi.Int8> sysname;
+
+  @ffi.Array.multi([_UTSNAME_BSD_LENGTH])
+  external ffi.Array<ffi.Int8> nodename;
+
+  @ffi.Array.multi([_UTSNAME_BSD_LENGTH])
+  external ffi.Array<ffi.Int8> release;
+
+  @ffi.Array.multi([_UTSNAME_BSD_LENGTH])
+  external ffi.Array<ffi.Int8> version;
+
+  @ffi.Array.multi([_UTSNAME_BSD_LENGTH])
+  external ffi.Array<ffi.Int8> machine;
+}
+
+extension _UtsnameBsd on ffi.Pointer<_utsname_bsd_t> {
+  Utsname toUtsname() => Utsname(
+        sysname: ref.sysname.toDartString(_UTSNAME_BSD_LENGTH),
+        nodename: ref.nodename.toDartString(_UTSNAME_BSD_LENGTH),
+        release: ref.release.toDartString(_UTSNAME_BSD_LENGTH),
+        version: ref.version.toDartString(_UTSNAME_BSD_LENGTH),
+        machine: ref.machine.toDartString(_UTSNAME_BSD_LENGTH),
+      );
+}
--- lib/src/util/conversions.dart
+++ lib/src/util/conversions.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:ffi' as ffi;
 
 import 'package:ffi/ffi.dart' as pffi;
@@ -50,4 +51,18 @@ String copyCBuffToDartString<T extends ffi.NativeType>(ffi.Pointer<T> cBuf,
   }
 
   return string;
+}
+
+extension Utf8String on ffi.Array<ffi.Int8> {
+  String toDartString(int maxLength) {
+    final codeUnits = asTypedList(maxLength).takeWhile((c) => c != 0);
+    return utf8.decode(codeUnits.toList());
+  }
+}
+
+extension TypedInt8List on ffi.Array<ffi.Int8> {
+  // https://github.com/dart-lang/sdk/issues/45508
+  List<int> asTypedList(int length) => <int>[
+        for (var i = 0; i < length; ++i) this[i],
+      ];
 }
